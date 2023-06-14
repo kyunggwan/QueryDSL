248d56f4a18145c912a510f710a17010ce2ae011
kyunggwan <4562sky@naver.com>
2023-06-14 14:14:25 +0900

    DB 설정

--- src/test/java/study/querydsl/StudyApplicationTests.java
+++ src/test/java/study/querydsl/StudyApplicationTests.java
@@ -5,16 +5,16 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
 
 import javax.persistence.EntityManager;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest
 @Transactional
+@Commit // Test는 Rollback 하기 때문에 저장된 것을 보려면 Commit
 class StudyApplicationTests {
 
 
@@ -33,8 +33,9 @@ class StudyApplicationTests {
 				.selectFrom(qHello)
 				.fetchOne();
 
-		assertThat(result).isEqualTo(hello);
-		assertThat(result.getId()).isEqualTo(hello.getId());
+        Assertions.assertThat(result).isEqualTo(hello);
+        // lombok 동작 확인 (hello.getId())
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
