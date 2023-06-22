6babceb315751a65860f9afea288918f0b698f58
kyunggwan <4562sky@naver.com>
2023-06-22 17:56:41 +0900

    feat: 순수 JPA 리포지토리와 Querydsl

---
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,46 @@
+package study.querydsl.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void basicTest(){
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+
+        Member findmember = memberJpaRepository.findById(member.getId()).get();
+        assertThat(findmember).isEqualTo(member);
+
+        List<Member> result1 = memberJpaRepository.findAll();
+        assertThat(result1).containsExactly(member);
+
+        List<Member> result2 = memberJpaRepository.findByUsername("member1");
+        assertThat(result2).containsExactly(member);
+
+        List<Member> result3 = memberJpaRepository.findAll_QueryDsl();
+        assertThat(result3).containsExactly(member);
+
+        List<Member> result4 = memberJpaRepository.findByUsername_QueryDsl("member1");
+        assertThat(result4).containsExactly(member);
+    }
+
+
+}
